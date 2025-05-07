24f5d2d8701e392a14cd7da6f83f58e536c76e3a
glhrip <220711809@students.uajy.ac.id>
2025-05-07 18:14:58 +0700

    be coba coba

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BarangTitipanController;
-
-// // Ini route API resource yang kamu butuhkan
-// Route::apiResource('barang_titipan', BarangTitipanController::class);
